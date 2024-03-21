7e1526ff49a15e7a8f61ac5ec6bf9752fa38b879
wahei628 <wahei628@gmail.com>
2024-03-21 15:10:09 +0900

    ホワイトリストに許可したいhostの許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # ホワイトリストに許可したいhostの追加
+  config.hosts << "outspire-c34a8ece51b3.herokuapp.com"
+  
 end
